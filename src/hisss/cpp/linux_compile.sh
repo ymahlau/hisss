de3d1bfda8ed9b2176bb5c25437f78f7d50ee9ad
ymahlau <mahlauyannik@gmail.com>
2025-12-09 15:02:04 +0100

    updated compilation scripts

--- src/hisss/cpp/linux_compile.sh
+++ src/hisss/cpp/linux_compile.sh
@@ -6,7 +6,7 @@ g++ -Wall -std=c++11 -c -fPIC -O3 source/battlesnake.cpp -o compiled/battlesnake
 g++ -Wall -std=c++11 -c -fPIC -O3 source/battlesnake_helper.cpp -o compiled/battlesnake_helper.o
 g++ -Wall -std=c++11 -c -fPIC -O3 source/utils.cpp -o compiled/utils.o
 g++ -Wall -std=c++11 -c -fPIC -O3 source/nash.cpp -o compiled/nash.o
-g++ -Wall -std=c++11 -c -fPIC -O3 link.cpp -o compiled/link.o
+g++ -Wall -std=c++11 -c -fPIC -O3 source/link.cpp -o compiled/link.o
 echo "Starting Linking..."
 g++ -Wall -std=c++11 -O3 -shared -Wl,-soname,-compiled/liblink.so -o compiled/liblink.so \
  compiled/ap.o \
--- src/hisss/cpp/windows_compile.bat
+++ src/hisss/cpp/windows_compile.bat
@@ -7,11 +7,7 @@ g++ -Wall -std=c++11 -c -fPIC -O3 source/battlesnake.cpp -o compiled/battlesnake
 g++ -Wall -std=c++11 -c -fPIC -O3 source/battlesnake_helper.cpp -o compiled/battlesnake_helper.o
 g++ -Wall -std=c++11 -c -fPIC -O3 source/utils.cpp -o compiled/utils.o
 g++ -Wall -std=c++11 -c -fPIC -O3 source/nash.cpp -o compiled/nash.o
-g++ -Wall -std=c++11 -c -fPIC -O3 source/logit.cpp -o compiled/logit.o
-g++ -Wall -std=c++11 -c -fPIC -O3 source/mle.cpp -o compiled/mle.o
-g++ -Wall -std=c++11 -c -fPIC -O3 source/quantal.cpp -o compiled/quantal.o
-g++ -Wall -std=c++11 -c -fPIC -O3 source/overcooked.cpp -o compiled/overcooked.o
-g++ -Wall -std=c++11 -c -fPIC -O3 link.cpp -o compiled/link.o
+g++ -Wall -std=c++11 -c -fPIC -O3 source/link.cpp -o compiled/link.o
 ECHO Starting Linking...
 g++ -Wall -std=c++11 -static -O3 -shared -Wl,-soname,-compiled/liblink.so -o compiled/liblink.so^
  compiled/ap.o^
@@ -21,11 +17,7 @@ g++ -Wall -std=c++11 -static -O3 -shared -Wl,-soname,-compiled/liblink.so -o com
  compiled/solvers.o^
  compiled/optimization.o^
  compiled/utils.o^
- compiled/logit.o^
  compiled/nash.o^
  compiled/battlesnake.o^
  compiled/battlesnake_helper.o^
- compiled/mle.o^
- compiled/quantal.o^
- compiled/overcooked.o^
  compiled/link.o
